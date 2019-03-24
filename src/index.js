5e9dbd115edd5c23c66056b0bdd59723eedf49c0
Katerina Zhadko <katerinkaj@icloud.com>
2019-03-24 17:31:31 +0300

    feat: add variables and if/for to calculate exchange from 50 to 1 ¢

--- src/index.js
+++ src/index.js
@@ -1,5 +1,66 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+
+    let exchangeResult = {};
+    exchangeResult.H; // worth 50¢
+    exchangeResult.Q; // worth 25¢
+    exchangeResult.D; // worth 10¢
+    exchangeResult.N; // worth 5¢
+    exchangeResult.P; // worth 1¢
+
+    if (currency <= 0) {
+        return exchangeResult;
+    }
+
+    if (currency > 10000) {
+        return { "error": "You are rich, my friend! We don't have so much coins for exchange" };
+    }
+
+    if (currency >= 50) {
+        exchangeResult.H = 0;
+    }
+
+    for (let i = 50; i <= currency;) {
+        exchangeResult.H++;
+        currency -= i;
+    }
+
+    if (currency >= 25) {
+        exchangeResult.Q = 0;
+    }
+
+    for (let i = 25; i <= currency;) {
+        exchangeResult.Q++;
+        currency -= i;
+    }
+
+    if (currency >= 10) {
+        exchangeResult.D = 0;
+    }
+
+    for (let i = 10; i <= currency;) {
+        exchangeResult.D++;
+        currency -= i;
+    }
+
+    if (currency >= 5) {
+        exchangeResult.N = 0;
+    }
+
+    for (let i = 5; i <= currency;) {
+        exchangeResult.N++;
+        currency -= i;
+    }
+
+    if (currency >= 1) {
+        exchangeResult.P = 0;
+    }
+
+    for (let i = 1; i <= currency;) {
+        exchangeResult.P++;
+        currency -= i;
+    }
+
+    return exchangeResult;
 }
